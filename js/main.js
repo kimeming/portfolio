71df123a5dda357bc6c85cc6a7c1c94fc83de107
kimeming <kimeming030@gmail.com>
2025-03-12 17:23:46 +0900

    작업중

--- js/main.js
+++ js/main.js
@@ -9,6 +9,50 @@ $(() => {
 
   requestAnimationFrame(raf);
 
+  // 마우스 커서
+  const cursor = document.querySelector(".cursor");
+
+  // 커서 이동
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+    cursor.animate(
+      {
+        top: `${e.clientY}px`,
+        left: `${e.clientX}px`,
+      },
+      1000
+    );
+  });
+
+  // 마우스 클릭
+  document.addEventListener("mousedown", () => {
+    cursor.classList.add("clicked");
+  });
+  document.addEventListener("mouseup", () => {
+    cursor.classList.remove("clicked");
+  });
+  // 링크 hover
+  let links = document.querySelectorAll("a");
+  links.forEach((i) => {
+    i.addEventListener("mouseover", () => {
+      cursor.classList.add("pointer");
+    });
+    i.addEventListener("mouseout", () => {
+      cursor.classList.remove("pointer");
+    });
+  });
+  // 버튼 hover
+  let btns = document.querySelectorAll("button");
+  btns.forEach((i) => {
+    i.addEventListener("mouseover", () => {
+      cursor.classList.add("pointer");
+    });
+    i.addEventListener("mouseout", () => {
+      cursor.classList.remove("pointer");
+    });
+  });
+
   // main gnb
   const $menuBtn = $(".menu-btn"),
     $gnbWrap = $(".gnb-wrap"),
